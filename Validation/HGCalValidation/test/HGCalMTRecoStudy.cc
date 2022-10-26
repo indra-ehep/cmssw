7653cc7569571398739d1f5d4a77784a56310eda
indra-ehep <77208636+indra-ehep@users.noreply.github.com>
2022-10-26 08:34:15 +0530

    Update Validation/HGCalValidation/test/HGCalMTRecoStudy.cc
    Feedback 7 of @perrotta
    
    Co-authored-by: Andrea Perrotta <perrotta@cern.ch>

--- Validation/HGCalValidation/test/HGCalMTRecoStudy.cc
+++ Validation/HGCalValidation/test/HGCalMTRecoStudy.cc
@@ -237,7 +237,6 @@ HGCalMTRecoStudy::HGCalMTRecoStudy(const edm::ParameterSet &iConfig)
         fs->make<TH1D>(Form("hELossLayer0_%02d", i + 1), Form("Rec:ELoss0 for layer %d", i + 1), 500, 0., 5000.));
     hELossLayer1.emplace_back(
         fs->make<TH1D>(Form("hELossLayer1_%02d", i + 1), Form("Rec:ELoss1 for layer %d", i + 1), 500, 0., 5000.));
-
     hNCellsLayer0.emplace_back(
         fs->make<TH1D>(Form("hNCellsLayer0_%02d", i + 1), Form("Rec:NCells0 for layer %d", i + 1), 200, -0.5, 199.5));
     hNCellsLayer1.emplace_back(
